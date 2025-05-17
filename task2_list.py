334b039970b595942c7c69cc4885369a61cd96ce
Muskan <muskanyav06132013@gmail.com>
2025-05-17 11:45:06 +0530

    Create task2_list.py

---
+++ task2_list.py
@@ -0,0 +1,4 @@
+orginal_list = [1,2,3,4,5,6,7,8,9,10]
+print("Original list:",orginal_list)
+print("Extracted first five elements:",orginal_list[0:5])
+print("Reversed extracted elements:",orginal_list[4::-1])
