8b2cd66cb6848ce21e9d3b66ebb7430f0034e5a7
Muskan <muskanyav06132013@gmail.com>
2025-05-17 11:50:02 +0530

    Update task1_dict_of_student.py

--- task1_dict_of_student.py
+++ task1_dict_of_student.py
@@ -1,6 +1,6 @@
-dict = {'Alice':85,'Mike':95,'Tom':80}
+student_marks = {'Alice':85,'Mike':95,'Tom':80}
 name = input("Enter the student's name: ")
-if name in dict:
-    print(name+"'s marks:",dict[name])
+if name in student_marks:
+    print(name+"'s marks:",student_marks[name])
 else:
     print("Student not found.")
